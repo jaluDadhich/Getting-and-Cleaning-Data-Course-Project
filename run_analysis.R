1fdaf8a0abb0f67ae74efcfed46885e300874040
Abhishek Sharma <eabhisharma25@gmail.com>
2016-08-29 07:16:15 +0100

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,91 @@
+filesPath <- "C:/Users/jb/Documents/Analytics course/coursera getting and cleaning data/course project/UCI HAR Dataset"
+setwd(filesPath)
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
+
+
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+
+
+library(dplyr)
+library(data.table)
+library(tidyr)
+
+
+filesPath <- "C:/Users/jb/Documents/Analytics course/coursera getting and cleaning data/course project/UCI HAR Dataset"
+
+dataSubjectTrain <- tbl_df(read.table(file.path(filesPath, "train", "subject_train.txt")))
+dataSubjectTest  <- tbl_df(read.table(file.path(filesPath, "test" , "subject_test.txt" )))
+
+
+dataActivityTrain <- tbl_df(read.table(file.path(filesPath, "train", "Y_train.txt")))
+dataActivityTest  <- tbl_df(read.table(file.path(filesPath, "test" , "Y_test.txt" )))
+
+
+dataTrain <- tbl_df(read.table(file.path(filesPath, "train", "X_train.txt" )))
+dataTest  <- tbl_df(read.table(file.path(filesPath, "test" , "X_test.txt" )))
+
+
+
+
+alldataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
+setnames(alldataSubject, "V1", "subject")
+alldataActivity<- rbind(dataActivityTrain, dataActivityTest)
+setnames(alldataActivity, "V1", "activityNum")
+
+
+dataTable <- rbind(dataTrain, dataTest)
+
+
+dataFeatures <- tbl_df(read.table(file.path(filesPath, "features.txt")))
+setnames(dataFeatures, names(dataFeatures), c("featureNum", "featureName"))
+colnames(dataTable) <- dataFeatures$featureName
+
+
+activityLabels<- tbl_df(read.table(file.path(filesPath, "activity_labels.txt")))
+setnames(activityLabels, names(activityLabels), c("activityNum","activityName"))
+
+# Merge columns
+alldataSubjAct<- cbind(alldataSubject, alldataActivity)
+dataTable <- cbind(alldataSubjAct, dataTable)
+
+
+# Reading "features.txt" and extracting only the mean and standard deviation
+dataFeaturesMeanStd <- grep("mean\\(\\)|std\\(\\)",dataFeatures$featureName,value=TRUE) #var name
+
+# Taking only measurements for the mean and standard deviation and add "subject","activityNum"
+
+dataFeaturesMeanStd <- union(c("subject","activityNum"), dataFeaturesMeanStd)
+dataTable<- subset(dataTable,select=dataFeaturesMeanStd) 
+
+
+
+##enter name of activity into dataTable
+dataTable <- merge(activityLabels, dataTable , by="activityNum", all.x=TRUE)
+dataTable$activityName <- as.character(dataTable$activityName)
+
+## create dataTable with variable means sorted by subject and Activity
+dataTable$activityName <- as.character(dataTable$activityName)
+dataAggr<- aggregate(. ~ subject - activityName, data = dataTable, mean) 
+dataTable<- tbl_df(arrange(dataAggr,subject,activityName))
+
+
+#Names before
+head(str(dataTable),2)
+
+
+names(dataTable)<-gsub("std()", "SD", names(dataTable))
+names(dataTable)<-gsub("mean()", "MEAN", names(dataTable))
+names(dataTable)<-gsub("^t", "time", names(dataTable))
+names(dataTable)<-gsub("^f", "frequency", names(dataTable))
+names(dataTable)<-gsub("Acc", "Accelerometer", names(dataTable))
+names(dataTable)<-gsub("Gyro", "Gyroscope", names(dataTable))
+names(dataTable)<-gsub("Mag", "Magnitude", names(dataTable))
+names(dataTable)<-gsub("BodyBody", "Body", names(dataTable))
+
+head(str(dataTable),6)
+
+
+write.table(dataTable, "TidyData.txt", row.name=FALSE)
+
